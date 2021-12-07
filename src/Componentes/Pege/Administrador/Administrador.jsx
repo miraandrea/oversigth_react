d6393734c640cce844b18370f50f53131a79e7d5
Elian-jaramillo-rojas <ejaramillo62@misena.edu.co>
2021-12-07 00:12:57 -0500

    interfaz de registrat

--- src/Componentes/Pege/Administrador/Administrador.jsx
+++ src/Componentes/Pege/Administrador/Administrador.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { Header1 } from "../../UI/Header1/Header1";
+import '../Administrador/Administrador.css'
 
 export const Administrador = () => {
     return (
-        <div>
+        <div className="body">  
             <Header1 />
-            <h1>Administrador</h1>
+            <h1>Registrarse</h1>
+            <div className="nombre_apellido">
+            <input type="text" placeholder="Nombre" />
+            <input type="text" placeholder="Apellidos" />
+
+            </div>
+            <div className="campos">
+
+            <input type="number" placeholder="Documento" />
+            <input type="password" placeholder="Contraseña" />
+            
+      
+            <select >
+            <option value="1">Rol</option> 
+            <option value="1">Dosente</option> 
+            <option value="2">Estudiante</option> 
+
+            </select>
+
+            <select >
+            <option value="1">Grado</option> 
+            <option value="1">6A</option> 
+            <option value="2">6B</option>
+            <option value="1">7A</option> 
+            <option value="2">7B</option> 
+            <option value="1">8A</option> 
+            <option value="2">8B</option> 
+            <option value="1">9A</option> 
+            <option value="2">9B</option> 
+            <option value="1">10A</option> 
+            <option value="2">10B</option>  
+
+            </select>
+            </div>
+            <button className="boton2">Registrarse </button>
+       
         </div>
     )
 }
